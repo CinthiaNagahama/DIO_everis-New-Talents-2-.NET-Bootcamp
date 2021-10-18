b1cd3f0ad5975ba7845b3c7469cc53e1a7a9f31f
Cinthia M. N. Ungefehr <ciin.nagahama@gmail.com>
2021-10-18 20:11:57 -0300

    Parts 1-3
    Partes 1-3 do projeto

---
+++ ApiGameCatalogue/ApiGameCatalogue/Controllers/v1/GameController.cs
@@ -0,0 +1,101 @@
+﻿using ApiGameCatalogue.InputModel;
+using ApiGameCatalogue.Services;
+using ApiGameCatalogue.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiGameCatalogue.Controllers.v1 
+{
+  [Route("api/V1/[controller]")]
+  [ApiController]
+  public class GameController : ControllerBase 
+  {
+    private readonly IGameService _gameService;
+
+    public GameController(IGameService gameService) 
+    {
+      _gameService = gameService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<GameViewModel>>> Get(
+      [FromQuery, Range(1, int.MaxValue)] int page = 1, 
+      [FromQuery, Range(1, 50)] int quantity = 5
+    ) {
+      var result = await _gameService.Get(page, quantity);
+
+      return result.Count == 0 ? NoContent() : Ok(result);
+    }
+
+    [HttpGet("{gameId:guid}")]
+    public async Task<ActionResult<GameViewModel>> Get([FromRoute] Guid gameId) 
+    {
+      var game = await _gameService.Get(gameId);
+
+      return game == null ? NoContent() : Ok(game);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<GameViewModel>> InsertGame([FromBody] GameInputModel inputGame) 
+    {
+      try
+      {
+        var game = await _gameService.Insert(inputGame);
+        return Ok(game);
+      } 
+      //catch(AlreadyRegisteredGameException ex)
+      catch(Exception)
+      {
+        return UnprocessableEntity("Já existe um jogo com esse nome para essa produtora");
+      }
+    }
+
+    [HttpPut("{gameId:guid}")]
+    public async Task<ActionResult> UpdateGame([FromRoute] Guid gameId, [FromBody] GameInputModel inputGame) {
+      try 
+      {
+        await _gameService.Update(gameId, inputGame);
+        return Ok();
+      }
+      //catch(NotRegisteredGameException ex)
+      catch (Exception) 
+      {
+        return UnprocessableEntity("Esse jogo não existe");
+      }
+    }
+
+    [HttpPatch("{gameId:guid}/{price:double}")]
+    public async Task<ActionResult> UpdateGame([FromRoute] Guid gameId, [FromRoute] double price) {
+      try 
+      {
+        await _gameService.Update(gameId, price);
+        return Ok();
+      }
+      //catch(NotRegisteredGameException ex)
+      catch (Exception) 
+      {
+        return UnprocessableEntity("Esse jogo não existe");
+      }
+    }
+
+    [HttpDelete("{gameId:guid}")]
+    public async Task<ActionResult> RemoveGame([FromRoute] Guid gameId) 
+    {
+      try 
+      {
+        await _gameService.Remove(gameId);
+        return Ok();
+      }
+      //catch(NotRegisteredGameException ex)
+      catch (Exception) 
+      {
+        return UnprocessableEntity("Esse jogo não existe");
+      }
+    }
+  }
+}
---
+++ ApiGameCatalogue/ApiGameCatalogue/InputModel/GameInputModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiGameCatalogue.InputModel 
+{
+  public class GameInputModel 
+  {
+    [Required]
+    [StringLength(100, MinimumLength = 1, ErrorMessage = "O nome do jogo deve conter entre 1 e 100 caracteres")]
+    public string Name { get; set; }
+
+    [Required]
+    [StringLength(100, MinimumLength = 1, ErrorMessage = "O nome da produtora deve conter entre 1 e 100 caracteres")]
+    public string Producer { get; set; }
+    
+    [Required]
+    [Range(1, 1000, ErrorMessage = "O preço do jogo deve ser de no mínimo R$1,00 e no máximo R$1000,00")]
+    public double Price { get; set; }
+  }
+}
